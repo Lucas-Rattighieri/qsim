fe521d0911f27b8d2e6dce82605b13c7f4ef4a67
Lucas Rattighieri <102265663+Lucas-Rattighieri@users.noreply.github.com>
2025-08-13 15:33:52 -0300

    Update Hy.py
    addition of the bitops parameter

--- qsim/hamiltonians/Hy.py
+++ qsim/hamiltonians/Hy.py
@@ -16,7 +16,7 @@ class Hy(Hamiltonian):
     """
 
 
-    def __init__(self, L: int, device="cpu", indices: torch.Tensor = None):
+    def __init__(self, L: int, device="cpu", bitops: BitOps = None, indices: torch.Tensor = None, tmp: torch.Tensor = None):
         """
         Initializes the Hy Hamiltonian for a system of L qubits.
 
@@ -24,7 +24,7 @@ class Hy(Hamiltonian):
             L (int): Number of qubits.
             device (str, optional): Device for tensor operations (default 'cpu').
         """
-        super().__init__(L, device, indices)
+        super().__init__(L, device, bitops, indices, tmp)
 
 
     def hamiltonian(self, psi, out=None):
