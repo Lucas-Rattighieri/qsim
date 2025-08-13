257d68f0ef902ec48ef9b4100f09edac3d42e58e
Lucas Rattighieri <102265663+Lucas-Rattighieri@users.noreply.github.com>
2025-08-13 14:40:24 -0300

    Update base.py
    addition of the create_temppsis and tpm parameters

--- qsim/hamiltonians/base.py
+++ qsim/hamiltonians/base.py
@@ -15,10 +15,12 @@ class Hamiltonian(ABC, Operators):
     """
 
 
-    def __init__(self, L: int, device = "cpu", indices: torch.Tensor = None):
-        super().__init__(L, device, indices)
-        self.tmppsi1 = torch.zeros(self.dim, dtype=self.dtype, device=device)
-        self.tmppsi2 = torch.zeros(self.dim, dtype=self.dtype, device=device)
+    def __init__(self, L: int, device = "cpu", indices: torch.Tensor = None, tmp: torch.Tensor = None, create_tmppsis = True):
+        super().__init__(L, device, indices, tmp)
+
+        if create_tmppsis:        
+            self.tmppsi1 = torch.zeros(self.dim, dtype=self.dtype, device=device)
+            self.tmppsi2 = torch.zeros(self.dim, dtype=self.dtype, device=device)
 
     
     @abstractmethod
