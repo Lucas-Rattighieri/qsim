3c4c7328f2ebb7a667117b6f98c4f41fbf345e0a
Lucas Rattighieri <102265663+Lucas-Rattighieri@users.noreply.github.com>
2025-08-18 14:52:20 -0300

    Update __init__.py
    addition of Htsp

--- qsim/__init__.py
+++ qsim/__init__.py
@@ -1,5 +1,5 @@
 from .bitops import BitOps
 from .operators import Operators
 from .states import States
-from .hamiltonians import Hx, Hy, Hz, Hamiltonian
+from .hamiltonians import Hx, Hy, Hz, Hamiltonian, Htsp
 from .diagonalops import DiagonalOps
