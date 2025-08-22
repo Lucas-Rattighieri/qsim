249506b0c8e34634d83cbf79e884339f2f231aa7
Lucas Rattighieri <102265663+Lucas-Rattighieri@users.noreply.github.com>
2025-08-22 16:49:44 -0300

    Update __init__.py
    addition of fqas

--- qsim/__init__.py
+++ qsim/__init__.py
@@ -4,3 +4,4 @@ from .states import States
 from .hamiltonians import Hx, Hy, Hz, Hamiltonian, Htsp
 from .diagonalops import DiagonalOps
 from .buffermanager import BufferManager
+from .fqas import falqon, trfalqon, sofalqon
