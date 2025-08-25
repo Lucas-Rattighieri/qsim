d228e1ec6d826197d641ed7bf0f946c7fb5cb967
Lucas Rattighieri <102265663+Lucas-Rattighieri@users.noreply.github.com>
2025-08-25 17:57:00 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="qsim",
+    version="0.1.0",
+    author="Lucas Rattighieri",
+    author_email="lucas.rattighieri@gmail.com",
+    description="Quantum circuit and algorithm simulation library using PyTorch",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Lucas-Rattighieri/qsim",
+    packages=find_packages(),
+    install_requires=[
+        "torch>=2.0.0"
+    ],
+    python_requires=">=3.10",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        # "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Physics",
+    ],
+)
