70e0fc4cec59e0ef8107f0df46d9a44ec9e1c5da
Lucas Rattighieri <102265663+Lucas-Rattighieri@users.noreply.github.com>
2025-09-03 19:59:23 -0300

    Update Hmaxcut.py

--- qsim/hamiltonians/problems/Hmaxcut.py
+++ qsim/hamiltonians/problems/Hmaxcut.py
@@ -96,9 +96,9 @@ class Hmaxcut(Hamiltonian):
                 identity_weigth += self.adjacency_matrix[vertex_i, vertex_j]
 
                 if self.fix_vertex and vertex_i == 0:
-                    self.diag.z_chain([vertex_j], self.adjacency_matrix[vertex_i, vertex_j], out=out_chain)
+                    self.diag.z_chain([index_j], self.adjacency_matrix[vertex_i, vertex_j], out=out_chain)
                 else:
-                    self.diag.z_chain([vertex_i, vertex_j], self.adjacency_matrix[vertex_i, vertex_j], out=out_chain)
+                    self.diag.z_chain([index_i, index_j], self.adjacency_matrix[vertex_i, vertex_j], out=out_chain)
 
                 self.diag_hamiltonian.add_(out_chain)
         
