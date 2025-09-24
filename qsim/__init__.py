3c2274ed89ec2d56cb998aa193efd24de9945eee
Lucas Rattighieri <102265663+Lucas-Rattighieri@users.noreply.github.com>
2025-09-24 17:06:02 -0300

    Update __init__.py

--- qsim/__init__.py
+++ qsim/__init__.py
@@ -1,7 +1,7 @@
 from .bitops import BitOps
 from .operators import Operators
 from .states import States
-from .hamiltonians import Hx, Hy, Hz, Hamiltonian, Htsp, Hmaxcut
+from .hamiltonians import Hx, Hy, Hz, Hamiltonian, Htsp, Hmaxcut, Hmst
 from .diagonalops import DiagonalOps
 from .buffermanager import BufferManager
 from .fqas import falqon, trfalqon, sofalqon
